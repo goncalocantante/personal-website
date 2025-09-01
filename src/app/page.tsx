1669d0df3aa83db30da9af44f64e38f1e5ba0cf2
Gonçalo Cantante <goncalocantante@gmail.com>
2025-09-01 15:49:50 +0100

    build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 import Hero from "./components/hero";
 import Resume from "./components/resume";
-import Contact from "./components/contact";
+// import Contact from "./components/contact";
 
 export default function Home() {
   return (
